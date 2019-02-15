862562d2fcac3c9e74dc17088934da9acb19df26
sahithyabathini5 <47119311+sahithyabathini5@users.noreply.github.com>
2019-02-15 15:26:46 -0400

    Update and rename task1.4.py to task1.4.py,task1.5.py

--- task1.4.py,task1.5.py
+++ task1.4.py,task1.5.py
@@ -12,3 +12,20 @@ def character_word_count(file):
             else:
                 hist[word]+=1
     print(hist)
+
+    
+    
+    
+    
+    
+    """""""""task1.5""""
+    file=open("Book3.txt")
+def starts_with_vow(file):
+	t=("a", "e", "i", "o", "u")
+	count=0
+	for line in file:
+		for word in line:
+			if word[0] in t:
+				count=count+1
+	return count
+print(starts_with_vow(file))
